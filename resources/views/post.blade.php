01284eb08aff5c391927a7d663fb0093ada7f277
deisibonilla <deisi@growingminds.nl>
2021-05-13 19:50:34 +0200

    :sparkles: Feature: initial commit.

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<script rel="script" src="/app.js"></script>
+
+<body>
+    <article>
+        <?= $post ?>
+    </article>
+</body>
+
