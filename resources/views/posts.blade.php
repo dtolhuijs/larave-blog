e58c851be47a9f7817a388edc878101b83934ca1
deisibonilla <deisi@growingminds.nl>
2021-06-13 18:57:38 +0200

    :sparkles: Feature: created post class

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,29 +4,10 @@
 <script rel="script" src="/js/app.js"></script>
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My first post</a></h1>
-        <p>Laravel is a web application framework with expressive, elegant syntax.
-            A web framework provides a structure and starting
-            point for creating your application, allowing you to focus on creating something
-            amazing while we sweat the details.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My second post</a></h1>
-        <p>Laravel is a web application framework with expressive, elegant syntax.
-            A web framework provides a structure and starting
-            point for creating your application, allowing you to focus on creating something
-            amazing while we sweat the details.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My third post</a></h1>
-        <p>Laravel is a web application framework with expressive, elegant syntax.
-            A web framework provides a structure and starting
-            point for creating your application, allowing you to focus on creating something
-            amazing while we sweat the details.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) :?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach;?>
 </body>
 
